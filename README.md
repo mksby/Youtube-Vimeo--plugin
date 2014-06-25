cceae013cbd8b372a003b85bb4651c1c455f7d75
Maxim Troitsky <maxim.troitsky@gmail.com>
2014-06-25 23:43:33 +0300

    edit api

--- README.md
+++ README.md
@@ -1,26 +1,36 @@
 #Youtube jquery plugin
 
-Плагин подключает youtube видео через HTML5 Video для основных браузеров. В IE8 подключается через флеш плеер. При отключенном флеш плеере в IE8 появляется сообщение об ошибке на русском языке.
+Plug connects youtube video via HTML5 Video for major browsers. In IE8 connected via flash player. If you disable flash player in IE8, an error message appears in Russian.
 
-##Пример использования
+##Usage
 
-``` javascript
-$('#video').youtube('DLq4pT_N1Wo');
+``` html
+<div data-youtube="DLq4pT_N1Wo"></div>
+<div data-youtube="id: DLq4pT_N1Wo, width: 500, height: 300"></div>
 ```
 
+Can use "data-youtube" or "youtube" attribute.
+
+``` html
+<div youtube="DLq4pT_N1Wo"></div>
+```
+
+or
+
 ``` javascript
-$('#video').youtube({
-  videoWidth: 500,
-  videoHeight: 300,
-  videoID: 'DLq4pT_N1Wo',
-  srcSwf: 'http://yandex.st/swfobject/2.2/swfobject.min.js',
-  srcFlashPlayer: 'http://www.adobe.com/go/getflashplayer',
-  srcImageFlashPlayer: 'http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif',
+$('div').youtube('DLq4pT_N1Wo');
+$('div').youtube({
+  id: 'DLq4pT_N1Wo',
+  width: 500,
+  height: 300,
+  swfSrc: 'http://yandex.st/swfobject/2.2/swfobject.min.js',
+  flashPlayerSrc: 'http://www.adobe.com/go/getflashplayer',
+  imageFlashPlayerSrc: 'http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif',
   errorMessage: 'Для просмотра содержимого вам необходимо установить Adobe Flash Player.',
-  classNameIframe: 'hhpp-iframevideo',
-  classNameMessage: 'hhpp-youtubeerror-a',
-  classNameMessageTx: 'hhpp-youtubeerror-a__tx',
-  tagNameMessage: 'span',
-  tagNameMessageTx: 'span'
+  iframeClassName: 'hhpp-iframevideo',
+  messageClassName: 'hhpp-youtubeerror-a',
+  messageTxClassName: 'hhpp-youtubeerror-a__tx',
+  messageTagName: 'span',
+  messageTxTagName: 'span'
 });
 ```
--- youtube.js
+++ youtube.js
@@ -1,34 +1,64 @@
-$.fn.youtube = function(params) {
-    var isNotIE8 = window.navigator.appVersion.toLowerCase().indexOf('msie 8.0') === -1,
-        videoWidth = params.width || $(this).width(),
-        videoHeight = params.height || $(this).height() || 300,
-        videoID = params.idVideo || params,
-        srcSwf = params.srcSwf || 'http://yandex.st/swfobject/2.2/swfobject.min.js',
-        srcFlashPlayer = params.srcSwf || 'http://www.adobe.com/go/getflashplayer',
-        srcImageFlashPlayer = params.srcSwf || 'http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif',
-        errorMessage = params.errorMessage || 'Для просмотра содержимого вам необходимо установить Adobe Flash Player.',
-        classNameIframe = params.classNameIframe || 'hhpp-iframevideo',
-        classNameMessage = params.classNameMessage || 'hhpp-youtubeerror-a',
-        classNameMessageTx = params.classNameMessageTx || 'hhpp-youtubeerror-a__tx',
-        tagNameMessage = params.tagNameMessage || 'span',
-        tagNameMessageTx = params.tagNameMessageTx || 'span',
-        tmpNotIE = '<iframe class="'+ classNameIframe +'" width=' + videoWidth + ' height=' + videoHeight + ' src="http://www.youtube.com/embed/' + videoID + '?rel=0&amp;autohide=1&amp;showinfo=0&amp;wmode=transparent" frameborder="0" allowfullscreen="allowfullscreen"></iframe>',
-        tmpIE = '<'+ tagNameMessage +' class="'+ classNameMessage +'"><a href="http://www.adobe.com/go/getflashplayer" target="_blank"><img alt="Get Adobe Flash player" src="'+ srcImageFlashPlayer +'"></a><'+ tagNameMessageTx +' class="'+ classNameMessageTx +'">'+errorMessage+'</'+ tagNameMessageTx +'></'+ tagNameMessage +'>',
-        swfParams = ['http://www.youtube.com/v/' + videoID + '?rel=0&amp;autohide=1&amp;showinfo=0&amp;wmode=transparent', this[0].id, '' + videoWidth, '' + videoHeight, '9.0.0', null, null, { 'allowfullscreen': 'true' }, null];
+// Youtube jquery plugin 
+;(function() {
+    var countVideo = 0;
 
-    var self = this;
+    $.fn.youtube = function(params) {
+        var IE8 = /msie 8.0/i.test(navigator.appVersion),
+            videoWidth = parseInt(params.width) || $(this).width(),
+            videoHeight = parseInt(params.height) || $(this).height() || 300,
+            videoID = params.id || params,
+            swfSrc = params.swfSrc || 'http://yandex.st/swfobject/2.2/swfobject.min.js',
+            flashPlayerSrc = params.swfSrc || 'http://www.adobe.com/go/getflashplayer',
+            imageFlashPlayerSrc = params.swfSrc || 'http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif',
+            errorMessage = params.errorMessage || 'Для просмотра содержимого вам необходимо установить Adobe Flash Player.',
+            iframeClassName = params.iframeClassName || 'hhpp-iframevideo',
+            messageClassName = params.messageClassName || 'hhpp-youtubeerror-a',
+            messageTxClassName = params.messageTxClassName || 'hhpp-youtubeerror-a__tx',
+            messageTagName = params.messageTagName || 'span',
+            messageTxTagName = params.messageTxTagName || 'span',
+            tmpNotIE = '<iframe class="'+ iframeClassName +'" width=' + videoWidth + ' height=' + videoHeight + ' src="http://www.youtube.com/embed/' + videoID + '?rel=0&amp;autohide=1&amp;showinfo=0&amp;wmode=transparent" frameborder="0" allowfullscreen="allowfullscreen"></iframe>',
+            tmpIE = '<'+ messageTagName +' class="'+ messageClassName +'"><a href="'+ flashPlayerSrc +'" target="_blank"><img alt="Get Adobe Flash player" src="'+ imageFlashPlayerSrc +'"></a><'+ messageTxTagName +' class="'+ messageTxClassName +'">'+errorMessage+'</'+ messageTxTagName +'></'+ messageTagName +'>';
 
-    if (isNotIE8) {
-        $(self).append(tmpNotIE);
-    } else {
-        var script = document.createElement('script');
-        script.src = srcSwf;
-        document.head.appendChild(script);
-        script.onload = function() {
-            $(self).append(tmpIE);
-            swfobject.embedSWF.apply(null, swfParams);
+        var self = this;
+
+        if (!IE8) {
+           $(self).append(tmpNotIE);
+        } else {
+            if (!this.id) this[0].id = new Date().getTime() + countVideo++;
+            var swfParams = ['http://www.youtube.com/v/' + videoID + '?rel=0&amp;autohide=1&amp;showinfo=0&amp;wmode=transparent', this[0].id, '' + videoWidth, '' + videoHeight, '9.0.0', null, null, { 'allowfullscreen': 'true' }, null],
+                script = document.createElement('script');
+                
+            script.src = swfSrc;
+            document.head.appendChild(script);
+            script.onload = function() {
+                $(self).append(tmpIE);
+                swfobject.embedSWF.apply(null, swfParams);
+            }
         }
+
+        return self;
+    };
+
+    var dataHandler = {
+        vocabulary: ['youtube', 'data-youtube'],
+        start: function(string) {
+            $('['+ string +']').length && $('['+ string +']').each(function(index, element) {
+               var attr = element.getAttribute(string);
+               if (/:/.test(attr)) {
+                   attr = element.getAttribute(string).split(/:|,/).map(function(element) {
+                       return element.replace(/^\s+|\'|\s+$/g, '')
+                   }).reduce(function(o, element, index, array) {
+                     (index % 2 === 0) && (o[element] = array[index + 1]);
+                     return o;
+                   }, {});
+               }
+               $(element).youtube(attr);
+            });
+        }
+    };
+
+    for(var i = 0, len = dataHandler.vocabulary.length; i < len; i += 1) {
+        dataHandler.start(dataHandler.vocabulary[i])
     }
 
-    return self;
-}
+}());
